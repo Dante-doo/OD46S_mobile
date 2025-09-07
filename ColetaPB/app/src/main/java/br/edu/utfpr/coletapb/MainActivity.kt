091b146e1a35b89108d381d4fcafcca5e7ba55a6
Dante-doo <anapaulavolpatto2004@gmail.com>
2025-09-07 19:57:01 -0300

    DriverRegister
    Criação do layout da página de cadastro do motorista
    Correções em nomes de campos

--- ColetaPB/app/src/main/java/br/edu/utfpr/coletapb/MainActivity.kt
+++ ColetaPB/app/src/main/java/br/edu/utfpr/coletapb/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //TESTES
-        setContentView(R.layout.activity_adm_register)
+        setContentView(R.layout.activity_driver_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
